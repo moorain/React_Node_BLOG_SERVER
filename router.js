55ca7b8ded7aa8758598bf2b406edf77ba6c3eff
morain <1062254476@qq.com>
2021-07-25 23:50:01 +0800

    fix:add weightjson

--- router.js
+++ router.js
@@ -193,4 +193,81 @@ router.get('/delete', async (ctx) => {
 })
 
 
+
+// 2.数据2
+
+// 2.1添加数据
+router.get('/addWeightData', async (ctx) => {
+    let weight = ctx.request.query.weight
+    if (!weight) {
+        ctx.body = {
+            success: false,
+            msg: '参数错误！'
+        }
+    }
+    const findJson = () => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path.join(__dirname, `public/data/pfweight.json`), function (err, data) {
+                if (err) {
+                    resolve({ code: -1, msg: '删除失败！' + err })
+                    return console.error(err);
+                }
+                let jsonData = data.toString();//将二进制的数据转换为字符串
+                if (!jsonData) {
+                    jsonData = JSON.stringify([]);
+                }
+
+                const arr = JSON.parse(jsonData);
+                const now = moment();
+
+                const obj = {
+                    id: now.valueOf(),
+                    date: now.format('YYYY-MM-DD HH:mm:SS'),
+                    weight,
+                }
+
+                const newJsonData = [obj, ...arr]
+                let str = JSON.stringify(newJsonData);
+
+                fs.writeFile(path.join(__dirname, 'public/data/pfweight.json'), str, function (err) {
+                    if (err) {
+                        resolve({ code: -1, msg: '更新失败！' + err })
+                    }
+                    resolve({
+                        success: true,
+                        msg: "更新成功！",
+                        data: newJsonData,
+                    });
+                })
+            })
+        })
+
+    }
+    ctx.body = await findJson();
+})
+
+router.get('/weightDatalList', async (ctx) => {
+    const findJson = () => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path.join(__dirname, `public/data/pfweight.json`), function (err, data) {
+                if (err) {
+                    resolve({ code: -1, msg: '删除失败！' + err })
+                    return console.error(err);
+                }
+                let jsonData = data.toString();//将二进制的数据转换为字符串
+                if (!jsonData) {
+                    jsonData = JSON.stringify([]);
+                }
+
+                const arr = JSON.parse(jsonData);
+                resolve({ success: true, code: 200, msg: '查询成功！', data: arr })
+            })
+        })
+
+    }
+    ctx.body = await findJson();
+})
+
+
+
 module.exports = router;
