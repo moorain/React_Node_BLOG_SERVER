92eb59ee41bcef56286019a5a58d09cbe5e73a66
morain <1062254476@qq.com>
2021-09-11 16:20:30 +0800

    fix:fixonline edit

--- routers/onlineEditor.js
+++ routers/onlineEditor.js
@@ -5,6 +5,51 @@ const jwt = require('jsonwebtoken');
 const router = new Router();
 const moment = require('moment');
 const util = require('../utils');
+// 更新json文件Func
+async function updateJson({ type, newData, id, pathStr }) {
+  // 更新json文件
+  const read = () => {
+    return new Promise((resolve) => {
+      fs.readFile(pathStr, function (err, data) {
+        resolve(data)
+      })
+    })
+  }
+
+  //写入json文件
+  const writeFiles = (str, writePath) => {
+    return new Promise((resolve) => {
+      fs.writeFile(writePath, str, function (err) {
+        resolve(util.res(null, true, '上传成功！'))
+      })
+    })
+  }
+  const data = await read();
+  let jsonData = data && data.toString();//将二进制的数据转换为字符串
+  if (!jsonData) {
+    jsonData = JSON.stringify([])
+  }
+  let str;
+  const Arr = JSON.parse(jsonData);
+  let isAdd = false;
+  //复写
+  Arr.forEach((i, index) => {
+    if (`${i.id}` === `${newData.id}`) {
+      Arr[index] = newData;
+      isAdd = true;
+    }
+  });
+
+  if (type === 'add' && (!isAdd)) {
+    const newJsonData = [newData, ...Arr];
+    str = JSON.stringify(newJsonData);
+  } else {
+    str = JSON.stringify(Arr);
+  }
+
+  const res = await writeFiles(str, pathStr)
+  return res
+}
 
 // 查询所有图片
 router.post('/codeChange', async (ctx) => {
@@ -12,6 +57,7 @@ router.post('/codeChange', async (ctx) => {
   const bodyStr = files['index.html'].value;
   // cdn
   let scriptStr = '';
+
   if (files.cdns.length > 0) {
     files.cdns.forEach((item) => {
       if (item.scripts.length > 0) {
@@ -30,10 +76,17 @@ router.post('/codeChange', async (ctx) => {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/antd@4.16.13/dist/antd.css" />
       <style>
       ${files['style.css'].value}
       </style>
-      ${scriptStr}
+      <script src="https://cdn.bootcdn.net/ajax/libs/react/16.13.1/umd/react.production.min.js"></script>
+      <script src="https://cdn.bootcdn.net/ajax/libs/react-dom/16.13.1/umd/react-dom.production.min.js"></script>
+      <script src="https://cdn.bootcdn.net/ajax/libs/babel-standalone/7.0.0-beta.3/babel.min.js"></script>
+
+      <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/antd@4.16.13/dist/antd.js"></script>
+  
     </head>
     <body>
       ${bodyStr}
@@ -50,6 +103,7 @@ router.post('/codeChange', async (ctx) => {
        }
       }, false ); 
       </script>
+      
     </body>
   </html>
   `
@@ -64,14 +118,84 @@ router.post('/codeChange', async (ctx) => {
     // await fs.writeFileSync(path.join(__dirname, '../public/editor/script.js'), jsbuffer)
     // await fs.writeFileSync(path.join(__dirname, '../public/editor/style.css'), cssbuffer)
 
+    //更新存档
+    const pathStr = path.join(__dirname, `../public/data/editor.json`)
+    // const read = () => {
+    //   return new Promise((resolve) => {
+    //     fs.readFile(pathStr, function (err, data) {
+    //       let jsonData = data && data.toString();//将二进制的数据转换为字符串
+    //       if (!jsonData) {
+    //         jsonData = JSON.stringify([])
+    //       }
+    //       resolve(JSON.parse(jsonData))
+    //     })
+    //   })
+    // }
+    // const data = await read();
+    // data.forEach((i) => {
+    //   if (i.id === files.key) {
+    //     // todo
+    //   }
+    // })
     ctx.body = util.res({
       key,
       url: `http://localhost:8001/${nameStr}`
     }, true, '更新成功！')
+
   } catch (err) {
     ctx.body = util.res([], false, '未知错误!')
   }
 })
+
+router.post('/codeSave', async (ctx) => {
+  const param = ctx.request.body;
+  const pathStr = path.join(__dirname, `../public/data/editor.json`)
+  await updateJson({
+    newData: param,
+    type: 'add',
+    id: param.id,
+    pathStr,
+  })
+  ctx.body = util.res(param.key, true, '保存成功!')
+})
+
+router.get('/codeQuery', async (ctx) => {
+  const { key } = ctx.request.query;
+  const pathStr = path.join(__dirname, `../public/data/editor.json`)
+  const read = () => {
+    return new Promise((resolve) => {
+      fs.readFile(pathStr, function (err, data) {
+        let jsonData = data && data.toString();//将二进制的数据转换为字符串
+        if (!jsonData) {
+          jsonData = JSON.stringify([])
+        }
+        resolve(JSON.parse(jsonData))
+      })
+    })
+  }
+  const data = await read();
+  const item = (data || []).find((i) => i.id === key);
+  ctx.body = util.res(item, true, '查询成功!')
+})
+
+
+router.get('/codeListQuery', async (ctx) => {
+  const pathStr = path.join(__dirname, `../public/data/editor.json`)
+  const read = () => {
+    return new Promise((resolve) => {
+      fs.readFile(pathStr, function (err, data) {
+        let jsonData = data && data.toString();//将二进制的数据转换为字符串
+        if (!jsonData) {
+          jsonData = JSON.stringify([])
+        }
+        resolve(JSON.parse(jsonData))
+      })
+    })
+  }
+  const data = await read();
+  ctx.body = util.res(data, true, '查询成功!')
+})
+
 
 
 module.exports = router;
